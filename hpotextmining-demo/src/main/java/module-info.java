adff3410ebd90ae25ff803d8222fa8616ad06ca0
Daniel Danis <daniel.danis@jax.org>
2021-06-10 17:54:44 -0400

    Modularize the `hpotextmining-demo` module

---
+++ hpotextmining-demo/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module hpotextmining.demo {
+    requires hpotextmining.gui;
+
+    requires phenol.core;
+    requires phenol.io;
+
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires org.slf4j;
+}
